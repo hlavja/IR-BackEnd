5a559c3e5ffe41038674cd19f7aca0a38dc907f4
hlavja <hlavjah@gmail.com>
2021-05-08 23:48:31 +0200

    Init

---
+++ src/test/java/cz/hlavja/ir/InformationRetrievalApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.hlavja.ir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationRetrievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
